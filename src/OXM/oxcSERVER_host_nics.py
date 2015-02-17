879f75cab1c909d7a09132cf9fa1055049935283
Daniel Lintott <daniel@serverb.co.uk>
2015-02-17 17:08:34 +0000

    PEP8 fixes

--- src/OXM/oxcSERVER_host_nics.py
+++ src/OXM/oxcSERVER_host_nics.py
@@ -16,7 +16,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 #
 # -----------------------------------------------------------------------
 
@@ -29,63 +30,65 @@ class oxcSERVERhostnics:
         if "Value" in res:
             self.track_tasks[res['Value']] = ref_vm
         else:
-            print res   
+            print res
         if delete_network:
-            res = self.connection.network.destroy(self.session_uuid, ref_network)
+            res = self.connection.network.destroy(self.session_uuid,
+                                                  ref_network)
             if "Value" in res:
                 self.track_tasks[res['Value']] = ref_vm
             else:
-                print res        
+                print res
 
-    def fill_available_nics(self, list, list2):
-        list.clear()
+    def fill_available_nics(self, list1, list2):
+        list1.clear()
         list2.clear()
         for pif_key in self.all['PIF'].keys():
             if self.all['PIF'][pif_key]['metrics'] != "OpaqueRef:NULL":
                 pif_metric = {}
-                if self.all['PIF'][pif_key]['metrics'] in self.all['PIF_metrics']:
+                if self.all['PIF'][pif_key]['metrics'] in \
+                        self.all['PIF_metrics']:
                     pif_metric = self.all['PIF_metrics'][self.all['PIF'][pif_key]['metrics']]
                 else:
                     pif_metric["pci_bus_path"] = "N/A"
             pif = self.all['PIF'][pif_key]
-            if self.all['PIF'][pif_key]['metrics'] != "OpaqueRef:NULL" and pif_metric['pci_bus_path'] != "N/A":
+            if self.all['PIF'][pif_key]['metrics'] != "OpaqueRef:NULL" \
+                    and pif_metric['pci_bus_path'] != "N/A":
                 nic = "NIC %s" % pif['device'][-1:]
                 error = ""
                 if len(self.all['network'][pif['network']]['VIFs']):
                     error = "in use by VMs"
-                if pif['bond_slave_of'] != "OpaqueRef:NULL" and pif['bond_slave_of'] in self.all['Bond']:
+                if pif['bond_slave_of'] != "OpaqueRef:NULL" \
+                        and pif['bond_slave_of'] in self.all['Bond']:
                     devices = []
-                    for slave in self.all['Bond'][pif['bond_slave_of']]['slaves']:
+                    for slave in \
+                            self.all['Bond'][pif['bond_slave_of']]['slaves']:
                             devices.append(self.all['PIF'][slave]['device'][-1:])
-                    devices.sort() 
+                    devices.sort()
                     error = "already in Bond %s" % ('+'.join(devices))
-                list.append([pif_key,nic,error,error == ""])
+                list1.append([pif_key, nic, error, error == ""])
 
-    def create_bond(self, ref, ref2, name, name2,auto=False):
-        network_cfg = {
-                'uuid' : '',
-                'name_label': "Bond %s+%s" % (name[-1:], name2[-1:]),
-                'name_description': '',
-                'VIFs': [],
-                'PIFs': [],
-                'other_config': {
-                        'XenCenterCreateInProgress': "true"
-                        },
-                'bridge': '',
-                'blobs': {}
-             }
+    def create_bond(self, ref, ref2, name, name2, auto=False):
+        network_cfg = {'uuid': '',
+                       'name_label': "Bond %s+%s" % (name[-1:], name2[-1:]),
+                       'name_description': '',
+                       'VIFs': [],
+                       'PIFs': [],
+                       'other_config': {'XenCenterCreateInProgress': "true"},
+                       'bridge': '',
+                       'blobs': {}}
         if auto:
             network_cfg['other_config']['automatic'] = "true"
         else:
             network_cfg['other_config']['automatic'] = "false"
-        res = self.connection.network.create(self.session_uuid, network_cfg) 
+        res = self.connection.network.create(self.session_uuid, network_cfg)
         if "Value" in res:
             self.track_tasks[res['Value']] = ref
             network = res['Value']
         else:
             print res
 
-        res = self.connection.Async.Bond.create(self.session_uuid, network, [ref, ref2],"")
+        res = self.connection.Async.Bond.create(self.session_uuid,
+                                                network, [ref, ref2], "")
         if "Value" in res:
             self.track_tasks[res['Value']] = ref
         else:
@@ -107,16 +110,15 @@ class oxcSERVERhostnics:
                 connected = "Connected"
         labels = {}
         labels['lblnicname'] = "NIC %s" % pif['device'][-1:]
-        labels['lblnicvendor'] =  pif_metric['vendor_name']
-        labels['lblnicdevice'] = pif_metric['device_name'] 
-        labels['lblnicmac'] =  mac
+        labels['lblnicvendor'] = pif_metric['vendor_name']
+        labels['lblnicdevice'] = pif_metric['device_name']
+        labels['lblnicmac'] = mac
         labels['lblnicpcibus'] = pif_metric['pci_bus_path']
-        labels['lblniclinkstatus'] = connected 
+        labels['lblniclinkstatus'] = connected
         if connected == "Connected":
             labels['lblnicspeed'] = pif_metric['speed'] + " mbit/s"
         else:
             labels['lblnicspeed'] = ""
-        labels['lblnicduplex'] =  duplex
+        labels['lblnicduplex'] = duplex
         for label in labels.keys():
             self.wine.builder.get_object(label).set_label(labels[label])
-
